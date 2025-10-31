d65760431554e553087c0786148639e7768dad50
Ceyhun Gökçe <cyhngkce@gmail.com>
2025-10-31 23:19:46 +0300

    first commit

---
+++ lib/pages/login_register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterPage extends StatefulWidget {
+  const LoginRegisterPage({super.key});
+
+  @override
+  State<LoginRegisterPage> createState() => _LoginRegisterPageState();
+}
+
+class _LoginRegisterPageState extends State<LoginRegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
